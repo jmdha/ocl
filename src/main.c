4f88d4bd180aa0fd269e1cb12b6bc75243825ef1
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2026-08-15 22:07:47 +0200

    feat: set port as arg

--- src/main.c
+++ src/main.c
@@ -11,14 +11,19 @@ static void signal_handler(int sig) {
     running = 0;
 }
 
-int main(void) {
+int main(int argc, char** argv) {
 	signal(SIGINT, signal_handler);   // Ctrl+C
 	signal(SIGTERM, signal_handler);  // kill <pid>
 					  
-	if (db_init() != 0)
+	if (db_init() != 0) {
+		fprintf(stderr, "failed to init db\n");
 		return 1;
-	if (web_init(8000) != 0)
+	}
+
+	if (web_init(atoi(argv[1])) != 0) {
+		fprintf(stderr, "failed to init web\n");
 		return 1;
+	}
 
 	while (running)
 		web_poll();
